2b31c5d0cd45770d9b0b0713e9ce5059095cb8c8
PraveenGithubCse <98269331+PraveenGithubCse@users.noreply.github.com>
2022-10-18 09:31:26 +0530

    initial commit

---
+++ Lab-Programs/LabProgram10/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseStateArray from './Functional Components/UseStateArray';
+
+function App() {
+  return (
+    <div className="App">
+      <UseStateArray/>
+    </div>
+  );
+}
+
+export default App;
